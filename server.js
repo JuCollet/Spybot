99d912471998ecaf32f371637e89943cc930332e
Julien <ju.collet@gmail.com>
2018-01-05 14:03:17 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+var gpio = require('gpio');
+
+var port = 8080;
+
+var left = {
+  forward: gpio.export(26, {ready: function(){}}),
+  backward: gpio.export(19, {ready: function(){}})
+}
+
+var right = {
+  forward: gpio.export(21, {ready: function(){}}),
+  backward: gpio.export(20, {ready: function(){ forward(); }}) 
+}
+
+function forward(){
+  left.forward.set();
+  right.forward.set();
+  setTimeout(function(){
+    stop();
+  }, 4000);
+}
+
+function stop(){
+  left.forward.reset();
+  left.backward.reset();
+  right.forward.reset();
+  right.backward.reset();
+  setTimeout(function(){
+    forward();
+  }, 10000)
+}
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+http.listen(port, function(){
+  console.log('listening on port ' + port);
+});
