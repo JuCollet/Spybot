b2c514f6f8cd27452e20935f0e4815e8b74d1b59
Julien <ju.collet@gmail.com>
2018-01-06 14:22:50 +0100

    remove gpio write tests

--- server.js
+++ server.js
@@ -16,26 +16,6 @@ lb.digitalWrite(0);
 rf.digitalWrite(0);
 rb.digitalWrite(0);
 
-forward();
-
-function forward(){
-  lf.digitalWrite(1);
-  rf.digitalWrite(1);
-  setTimeout(function(){
-    stop();
-  }, 4000);
-}
-
-function stop(){
-  lf.digitalWrite(0);
-  lb.digitalWrite(0);
-  rf.digitalWrite(0);
-  rb.digitalWrite(0);
-  setTimeout(function(){
-    forward();
-  }, 10000)
-}
-
 app.use(express.static(path.join(__dirname, '/client')));
 
 app.get('/', function(req, res){
